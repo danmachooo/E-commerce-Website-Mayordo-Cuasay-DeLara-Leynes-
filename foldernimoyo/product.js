e9970d9949206878fab4659d06d54cb95c6f9d1b
danmachooo <144467153+danmachooo@users.noreply.github.com>
2024-10-05 00:56:57 +0800

    Update product.js

--- foldernimoyo/product.js
+++ foldernimoyo/product.js
@@ -1,38 +1,29 @@
-const { pool } = require('../config/db'); // Adjust the path as necessary
+const { pool } = require('../config/db');
 
 const Product = {
-    // Method to create a new product
-    create: async (name, description, price, image, stock_quantity, category_id) => {
+    create: async (name, description, price, image, stockQuantity, categoryId) => {
         await pool.query(
             'INSERT INTO products (name, description, price, image, stock_quantity, category_id) VALUES (?, ?, ?, ?, ?, ?)', 
-            [name, description, price, image, stock_quantity, category_id]
+            [name, description, price, image, stockQuantity, categoryId]
         );
     },
 
-    // Method to find all products
+    findById: async (productId) => {
+        const [rows] = await pool.query('SELECT * FROM products WHERE id = ?', [productId]);
+        return rows[0]; // Return the product object if found
+    },
+
     findAll: async () => {
         const [rows] = await pool.query('SELECT * FROM products');
         return rows; // Return all products
     },
 
-    // Method to find a product by ID
-    findById: async (id) => {
-        const [rows] = await pool.query('SELECT * FROM products WHERE id = ?', [id]);
-        return rows[0]; // Return the product object if found
+    updateStock: async (productId, stockQuantity) => {
+        await pool.query('UPDATE products SET stock_quantity = ? WHERE id = ?', [stockQuantity, productId]);
     },
 
-    // Method to update a product by ID
-    update: async (id, updatedProduct) => {
-        const { name, description, price, image, stock_quantity, category_id } = updatedProduct;
-        await pool.query(
-            'UPDATE products SET name = ?, description = ?, price = ?, image = ?, stock_quantity = ?, category_id = ? WHERE id = ?', 
-            [name, description, price, image, stock_quantity, category_id, id]
-        );
-    },
-
-    // Method to delete a product by ID
-    delete: async (id) => {
-        await pool.query('DELETE FROM products WHERE id = ?', [id]);
+    delete: async (productId) => {
+        await pool.query('DELETE FROM products WHERE id = ?', [productId]);
     }
 };
 
